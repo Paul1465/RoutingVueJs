82161fcac2e987170dca2f6d2085593306c17ab7
Paul1465 <you@example.com>
2021-01-18 10:12:36 +0100

    first commit

---
+++ src/components/Accueil.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Voici l'accueil</h1>
+</template>
+
+<script>
+export default {
+    name: 'Accueil'
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,54 +1,28 @@
 <template>
-    <div class="header-top">
-        <p> {{titre}} </p>
+    <div>
+        <ul class="nav">
+            <li class="btn btn-primary btn-nav"><router-link to="/" exact>Accueil</router-link></li>
+            <li class="btn btn-primary btn-nav"><router-link to="/page1">Page1</router-link></li>
+            <li class="btn btn-primary btn-nav"><router-link to="/page2">Page2</router-link></li>
+        </ul>
     </div>
 </template>
 
 <script>
-    import {bus} from '../main'
-    export default {
-        name: 'Header',
-        data(){
-            return {
-                titre: 'Mon Titre de base'
-            }
-        },
-        beforeCreate(){
-            console.log('Il va être créé !');
-        },
-        created(){
-            console.log('Le composant est créé !');
-            bus.$on('changeTitre', (data) => {
-                this.titre = data
-            })
-        },
-        beforeMount(){
-            console.log("Il va être monté dans le DOM");
-        },
-        mounted(){
-            console.log("Il est intégré dans le DOM")
-        },
-        beforeUpdate(){
-            console.log('Il va se mettre à jour');
-        },
-        updated(){
-            console.log("Le composant s'est mis à jour");
-        }
-    }
+
+export default {
+    name: 'Header',
+}
 </script>
 
-<style scoped>
-
-    .header-top {
-        position: fixed;
-        top: 0;
-        height: 50px;
-        background: #333;
-        color: #f1f1f1;
-        width: 100%;
-        text-align: center;
-        line-height: 50px;
-        font-size: 25px;
+<style>
+    .btn-nav {
+        margin: 5px;
     }
-
+    .btn-nav a{
+        color: #fff;
+    }
+    .btn-nav a:hover{
+        color: #fff;
+    }
 </style>
